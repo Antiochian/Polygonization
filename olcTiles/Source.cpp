f0213a9c1002a6dd4daf0ee5f1c46be1866e2e0b
Henry Griffiths <trin3161@ox.ac.uk>
2020-08-16 22:42:16 +0100

    Refactored algorithm, added visual grid display

--- olcTiles/Source.cpp
+++ olcTiles/Source.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #include <map>
 
-constexpr int nSize = 10;
+constexpr int nSize = 10; //number of tiles per side
+const int nTileSize = 10; //number of pixels per tile_side
 const int nCellNumber = nSize*nSize; //hardcoded until i learn how this shit works lol
 
 class Cell {
@@ -38,15 +39,81 @@ public:
 public:
 	std::vector<Cell> CellList;
 	void ConvertTilesToPolygons() {
+
+		for (int i = 0; i < nCellNumber; i++) {
+			Cell* candidate = &CellList[i];
+
+			bool north_neighbour = ((*candidate).cy != 0) && (CellList[i - (nSize+1)].CellExists);
+			bool west_neighbour = ((*candidate).cx != 0) && (CellList[i - 1].CellExists);
+			bool self_exist = (*candidate).CellExists;
+
+			/*there are four possibilities for a northern edge to exist:
+				1: !north !west exist
+				2: !north west exist
+				3. north !west !exist
+				4. north west !exist
+
+			ie:
+				bool edge_exist = exist && !north || !exist && north
+
+			for a western edge to exist:
+				bool edge_exist = exist && !west || !exist &&  west
+			*/
+
+			if ((north_neighbour && !self_exist) || (!north_neighbour && self_exist)) {
+				(*candidate).EdgeExists[0] = true;
+				//now check if its a new edge or a continuation of an old edge
+				if ( ( (*candidate).cx != 0) && (CellList[i - 1].CellExists) && CellList[i-1].EdgeExists[0]) {
+					//Continue existing edge - get edge ID and edit endpoint
+					int neighbour_ID = CellList[i - 1].EdgeIDs[0];
+					Cell::EdgePool[neighbour_ID][2] += 1;
+					(*candidate).EdgeIDs[0] = neighbour_ID; //update current edge id to match
+
+				}
+				else {
+					//no continuation, add new edge to EDGE POOL
+					int edge_iter[] = { (*candidate).cx, (*candidate).cy, (*candidate).cx + 1, (*candidate).cy };
+					std::vector<int> new_vec(edge_iter, edge_iter + sizeof(edge_iter) / sizeof(int));
+					Cell::EdgePool.insert(std::pair<int, std::vector<int>>(nextID, new_vec));
+					//advance ID number
+					(*candidate).EdgeIDs[0] = nextID;
+					nextID += 1;
+				}
+			}
+
+
+				//check if there is NO western neighbour (all as before)
+				if ((west_neighbour && !self_exist) || (!west_neighbour && self_exist)) {
+					(*candidate).EdgeExists[0] = true;
+					if (((*candidate).cy != 0) && (CellList[i - (nSize + 1)].CellExists) && CellList[i - (nSize + 1)].EdgeExists[3]) {
+						//continuation of above edge - get edge ID and edit endpoint
+						int neighbour_ID = CellList[i - (nSize + 1)].EdgeIDs[3];
+						Cell::EdgePool[neighbour_ID][3] += 1;
+						(*candidate).EdgeIDs[3] = neighbour_ID; //update current edge id to match
+					}
+					else if ((*candidate).CellExists) {
+						//no continuation, add new edge to EDGE POOL
+						int edge_iter[] = { (*candidate).cx, (*candidate).cy, (*candidate).cx, (*candidate).cy+1};
+						std::vector<int> new_vec(edge_iter, edge_iter + sizeof(edge_iter) / sizeof(int));
+						Cell::EdgePool.insert(std::pair<int, std::vector<int>>(nextID, new_vec));
+						(*candidate).EdgeIDs[3] = nextID;
+						//advance ID number
+						nextID += 1;
+					}
+				//}
+				
+				
+			}
+		}
+
 		//add 4 borders of screen
-		//Cell::EdgePool
 		std::vector<int> border_vec;
 		int left_border[] = { 0, 0, 0, nSize };
 		int top_border[] = { 0, 0, nSize, 0 };
 		int right_border[] = { nSize, 0, nSize, nSize };
 		int bottom_border[] = { 0, nSize, nSize, nSize };
 
-		border_vec = std::vector<int> (left_border, left_border + sizeof(left_border) / sizeof(int));
+		border_vec = std::vector<int>(left_border, left_border + sizeof(left_border) / sizeof(int));
 		Cell::EdgePool.insert(std::pair<int, std::vector<int>>(nextID, border_vec));
 		nextID += 1;
 		border_vec = std::vector<int>(right_border, right_border + sizeof(right_border) / sizeof(int));
@@ -58,97 +125,6 @@ public:
 		border_vec = std::vector<int>(bottom_border, bottom_border + sizeof(bottom_border) / sizeof(int));
 		Cell::EdgePool.insert(std::pair<int, std::vector<int>>(nextID, border_vec));
 		nextID += 1;
-
-		for (int i = 0; i < nCellNumber; i++) {
-			Cell* candidate = &CellList[i];
-			if ((*candidate).CellExists) {
-				//check if there is NO northern neighbour
-				if (((*candidate).cy != 0) && (!CellList[i - 10].CellExists)) {
-					(*candidate).EdgeExists[0] = true;
-					//now check if its a new edge or a continuation of an old edge
-					if ( ((*candidate).cx != 0) && (CellList[i - 1].CellExists) && CellList[i-1].EdgeExists[0]) {
-						//Continue existing edge - get edge ID and edit endpoint
-						int neighbour_ID = CellList[i - 1].EdgeIDs[0];
-						Cell::EdgePool[neighbour_ID][2] = (*candidate).cx+1;
-						Cell::EdgePool[neighbour_ID][3] = (*candidate).cy;
-						(*candidate).EdgeIDs[0] = neighbour_ID; //update current edge id to match
-
-					}
-					else {
-						//no continuation, add new edge to EDGE POOL
-						int edge_iter[] = { (*candidate).cx, (*candidate).cy, (*candidate).cx + 1, (*candidate).cy };
-						std::vector<int> new_vec(edge_iter, edge_iter + sizeof(edge_iter) / sizeof(int));
-						Cell::EdgePool.insert(std::pair<int, std::vector<int>>(nextID, new_vec));
-						//advance ID number
-						(*candidate).EdgeIDs[0] = nextID;
-						nextID += 1;
-					}
-				}
-				//check if there is NO western neighbour (all as before)
-				if (((*candidate).cx != 0) && (!CellList[i - 1].CellExists)) {
-					(*candidate).EdgeExists[3] = nextID;
-					if (((*candidate).cy != 0) && (CellList[i - 10].CellExists) && CellList[i - 10].EdgeExists[3]) {
-						//continuation of above edge - get edge ID and edit endpoint
-						int neighbour_ID = CellList[i - 10].EdgeIDs[3];
-						Cell::EdgePool[neighbour_ID][2] = (*candidate).cx;
-						Cell::EdgePool[neighbour_ID][3] = (*candidate).cy + 1;
-						(*candidate).EdgeIDs[3] = neighbour_ID; //update current edge id to match
-					}
-					else {
-						//no continuation, add new edge to EDGE POOL
-						int edge_iter[] = { (*candidate).cx, (*candidate).cy, (*candidate).cx, (*candidate).cy+1};
-						std::vector<int> new_vec(edge_iter, edge_iter + sizeof(edge_iter) / sizeof(int));
-						Cell::EdgePool.insert(std::pair<int, std::vector<int>>(nextID, new_vec));
-						(*candidate).EdgeIDs[3] = nextID;
-						//advance ID number
-						nextID += 1;
-					}
-				}
-				//check if there is NO eastern neighbour (all as before)
-				if (((*candidate).cx != nSize-1) && (!CellList[i + 1].CellExists)) {
-					(*candidate).EdgeExists[1] = nextID;
-					if (((*candidate).cy != 0) && (CellList[i - 10].CellExists) && CellList[i - 10].EdgeExists[1]) {
-						//continuation of above edge - get edge ID and edit endpoint
-						int neighbour_ID = CellList[i - 10].EdgeIDs[3];
-						Cell::EdgePool[neighbour_ID][2] = (*candidate).cx+1;
-						Cell::EdgePool[neighbour_ID][3] = (*candidate).cy + 1;
-						(*candidate).EdgeIDs[1] = neighbour_ID; //update current edge id to match
-					}
-					else {
-						//no continuation, add new edge to EDGE POOL
-						int edge_iter[] = { (*candidate).cx+1, (*candidate).cy, (*candidate).cx+1, (*candidate).cy + 1 };
-						std::vector<int> new_vec(edge_iter, edge_iter + sizeof(edge_iter) / sizeof(int));
-						Cell::EdgePool.insert(std::pair<int, std::vector<int>>(nextID, new_vec));
-						(*candidate).EdgeIDs[1] = nextID;
-						//advance ID number
-						nextID += 1;
-					}
-				}
-				//check if there is NO southern neighbour
-				if (((*candidate).cy != nSize-1) && (!CellList[i + 10].CellExists)) {
-					(*candidate).EdgeExists[2] = true;
-					//now check if its a new edge or a continuation of an old edge
-					if (((*candidate).cx != 0) && (CellList[i - 1].CellExists) && CellList[i - 1].EdgeExists[2]) {
-						//Continue existing edge - get edge ID and edit endpoint
-						int neighbour_ID = CellList[i - 1].EdgeIDs[2];
-						Cell::EdgePool[neighbour_ID][2] = (*candidate).cx + 1;
-						Cell::EdgePool[neighbour_ID][3] = (*candidate).cy+1;
-						(*candidate).EdgeIDs[2] = neighbour_ID; //update current edge id to match
-
-					}
-					else {
-						//no continuation, add new edge to EDGE POOL
-						int edge_iter[] = { (*candidate).cx, (*candidate).cy+1, (*candidate).cx + 1, (*candidate).cy+1 };
-						std::vector<int> new_vec(edge_iter, edge_iter + sizeof(edge_iter) / sizeof(int));
-						Cell::EdgePool.insert(std::pair<int, std::vector<int>>(nextID, new_vec));
-						//advance ID number
-						(*candidate).EdgeIDs[2] = nextID;
-						nextID += 1;
-					}
-				}
-				
-			}
-		}
 	}
 
 	bool OnUserCreate() override
@@ -158,7 +134,7 @@ public:
 		bool texists;
 		int tx, ty;
 		srand(0);
-		for (int i = 0; i < nCellNumber; i++) {
+		for (int i = 0; i < (nSize+1)*(nSize+1); i++) {
 			//texists = ((rand() % 256) < 32) ? true : false;
 			if ((24 < i && i < 27) || (34 < i && i < 36)) {
 				texists = true;
@@ -166,8 +142,8 @@ public:
 			else {
 				texists = false;
 			}
-			int tx = i % 10;
-			int ty = i / 10;
+			int tx = i % (nSize+1);
+			int ty = i / (nSize+1);
 			CellList.push_back(Cell(texists, tx, ty));
 		}
 
@@ -178,7 +154,7 @@ public:
 		std::cout << "EDGE LIST: " << std::endl;
 		for (int n = 0; n < Cell::EdgePool.size(); n++){
 			std::cout << std::endl;
-			std::cout << Cell::EdgePool[n][0] << ", " << Cell::EdgePool[n][1] << " -->" << Cell::EdgePool[n][2] << ", " << Cell::EdgePool[n][3];	
+			std::cout << Cell::EdgePool[n][0] << ", " << Cell::EdgePool[n][1] << " --> " << Cell::EdgePool[n][2] << ", " << Cell::EdgePool[n][3];	
 		}
 		
 		return true;
@@ -190,10 +166,24 @@ public:
 		for (int i = 0; i < nCellNumber; i++) {
 			Cell* candidate = &CellList[i];
 			if ((*candidate).CellExists){
-				Draw((*candidate).cx, (*candidate).cy, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+				olc::Pixel col(rand() % 256, rand() % 256, rand() % 256);
+				for (int jx = 0; jx < nTileSize; jx++) {
+					for (int jy = 0; jy < nTileSize; jy++) {
+						Draw((*candidate).cx*nTileSize + jx, (*candidate).cy*nTileSize + jy, col );
+					}
+				}
+				
 			}
 		}
 		
+		for (int ix = 0; ix < nSize * nTileSize; ix++) {
+			for (int iy = 0; iy < nSize * nTileSize; iy++) {
+				if (!(ix%10) || !(iy%10)) {
+					Draw(ix, iy);
+
+				}
+			}
+		}
 		return true;
 	}
 };
@@ -201,7 +191,7 @@ public:
 int main()
 {
 	Tiler app;
-	if (app.Construct(nSize, nSize, 40, 40))
+	if (app.Construct(nSize*nTileSize, nSize*nTileSize, nTileSize, nTileSize))
 		app.Start();
 	return 0;
 }
